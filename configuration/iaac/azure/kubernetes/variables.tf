938f720e1081df4a657c63a59be651675ab13ffb
koko lubilubi <kokolubilubi@gmail.com>
2022-08-01 22:43:45 +0200

    updating image name

--- configuration/iaac/azure/kubernetes/variables.tf
+++ configuration/iaac/azure/kubernetes/variables.tf
@@ -1,29 +1,41 @@
 variable client_id {}
+
 variable client_secret {}
+
 variable ssh_public_key {}
 
 variable environment {
-    default = "dev"
+
+default = "dev"
+
 }
 
 variable location {
-    default = "westeurope"
+
+default = "westeurope"
+
 }
 
 variable node_count {
-  default = 3
+
+default = 1
+
 }
 
-
-
 variable dns_prefix {
-  default = "k8stest"
+
+default = "k8stest"
+
 }
 
 variable cluster_name {
-  default = "k8stest"
+
+default = "k8stest"
+
 }
 
 variable resource_group {
-  default = "kubernetes"
+
+default = "kubernetes"
+
 }
